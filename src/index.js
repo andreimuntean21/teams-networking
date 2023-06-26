824a3a1de08568e2c3298870a8fe16ea3e8df7e3
Andrei Muntean <andrei.muntean@fidasolutions.com>
2023-06-26 19:13:28 +0300

    delete team request

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ function $(selector) {
   return document.querySelector(selector);
 }
 
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: id })
+  }).then(r => r.json());
+}
+
 function getTeamAsHtml(team) {
   return `<tr>
 
@@ -15,7 +25,10 @@ function getTeamAsHtml(team) {
 
     <td>${team.url}</td>
 
-    <td>❌ &#9998;</td>
+    <td>
+    <a data-id="${team.id}" class="remove-btn">✖</a>
+    <a data-id="${team.id}" class="edit-btn">&#9998;</a>
+    </td>
 
     </tr>`;
 }
@@ -40,4 +53,20 @@ function loadTeams() {
     });
 }
 
+function initEvents() {
+  $("#teamsTable tbody").addEventListener("click", e => {
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id;
+      //console.warn("remove %s", id);
+      deleteTeamRequest(id).then(status => {
+        if (status.success) {
+          //console.warn("delete done", status);
+          loadTeams();
+        }
+      });
+    }
+  });
+}
+
 loadTeams();
+initEvents();

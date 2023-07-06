23534664c22151709fb08be14362aaae59282ee5
Andrei Muntean <andrei.muntean@fidasolutions.com>
2023-07-06 21:09:44 +0300

    allow to pass String or Element on mask/unmask

--- src/utilities.js
+++ src/utilities.js
@@ -2,11 +2,27 @@ export function $(selector) {
   return document.querySelector(selector);
 }
 
+/**
+ *
+ * @param {String|Element} el
+ */
+
 export function mask(el) {
-  el.classList.add("loading-mask");
+  if (typeof el == "string") {
+    el = $(el);
+  }
+  el && el.classList.add("loading-mask");
 }
 
+/**
+ *
+ * @param {String|Element} el
+ */
+
 export function unmask(el) {
+  if (typeof el == "string") {
+    el = $(el);
+  }
   el.classList.remove("loading-mask");
 }
 
@@ -23,3 +39,14 @@ export function sleep(ms) {
   await sleep(2000);
   console.warn("2. ready to do %o", "next job");
 })();
+
+export function filterElements(elements, search) {
+  search = search.toLowerCase();
+  return elements.filter(element => {
+    return Object.entries(element).some(([key, value]) => {
+      if (key !== "id") {
+        return value.toLowerCase().includes(search);
+      }
+    });
+  });
+}

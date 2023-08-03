88a448668287adbca876a946e17f53c5303685a3
Andrei Muntean <andrei.muntean@fidasolutions.com>
2023-08-03 10:07:02 +0300

    select all

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { deleteTeamRequest, updateTeamRequest, createTeamRequest, loadTeamsRequest } from "./middleware";
-import { $, debounce, filterElements, mask, sleep, unmask } from "./utilities";
+import { $, $$, debounce, filterElements, mask, sleep, unmask } from "./utilities";
 //import { debounce } from "lodash"; //bad - don't import all functions
 //import debounce from "lodash/debounce"; //better
 
@@ -134,7 +134,7 @@ async function onSubmit(e) {
 async function removeSelected() {
   mask("#main");
   //console.time("remove");
-  const selected = document.querySelectorAll("input[name=selected]:checked");
+  const selected = $$("input[name=selected]:checked");
   const ids = [...selected].map(input => input.value);
   const promises = ids.map(id => deleteTeamRequest(id));
   promises.push(sleep(2000));
@@ -162,6 +162,12 @@ function initEvents() {
       displayTeams(teams);
     }, 400)
   );
+
+  $("#selectAll").addEventListener("input", e => {
+    $$("input[name=selected]").forEach(check => {
+      check.checked = e.target.checked;
+    });
+  });
 
   $("#teamsTable tbody").addEventListener("click", e => {
     if (e.target.matches("a.remove-btn")) {
